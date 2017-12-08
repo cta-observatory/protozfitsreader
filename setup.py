c081836c2ed0273e28d11e7bb017ef923266e74a
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 10:40:12 +0100

    typo

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/bin/python
 from setuptools import setup
 setup(
-    name='protzfitsreader',
+    name='protozfitsreader',
     version='0.41',
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",
     data_files=[(
-        'protzfitsreader', [
-            'protzfitsreader/*.so*',
+        'protozfitsreader', [
+            'protozfitsreader/*.so*',
         ])
     ],
     install_requires=['numpy', 'protobuf'],
-    packages=['protzfitsreader'],
+    packages=['protozfitsreader'],
 )

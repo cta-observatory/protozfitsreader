91529587d2121ae345b9871c37bcd8bc1e6808ae
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 10:51:08 +0100

    maybe like this?

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",
-    package_data={'': ['*.so*']},
+    package_data={'': ['*.so']},
     install_requires=['numpy', 'protobuf'],
 )

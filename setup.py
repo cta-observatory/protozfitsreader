813ba92972c45e985b28c17d01cd962670a95866
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 10:46:20 +0100

    package_data must be dict

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@
 from setuptools import setup
 setup(
     name='protozfitsreader',
+    packages=['protozfitsreader'],
     version='0.41',
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",
-    package_data=[(
-        'protozfitsreader', [
-            'shared/*.so*',
-        ])
-    ],
+    package_data={'protozfitsreader': ['shared/*.so*']},
     install_requires=['numpy', 'protobuf'],
-    packages=['protozfitsreader'],
 )

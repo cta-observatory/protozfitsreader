0036b29369cf504e6b9922615227ae6ec7a4e0d7
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 11:00:13 +0100

    wrong glob

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",
-    package_data={'protozfitsreader': ['shared/*.so']},
+    package_data={'protozfitsreader': ['shared/*.so*']},
     install_requires=['numpy', 'protobuf'],
 )

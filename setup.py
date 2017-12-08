af16dc2bc58e9fd7333f2da584dd2c4592ce5db5
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 10:00:00 +0100

    use setuptools instead of distutils

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/bin/python
+from setuptool import setup
+setup(
+    name='ProtoZFitsReader',
+    version='0.41',
+    description='Basic python bindings for protobuf zfits reader',
+    author="Etienne Lyard et al.",
+    author_email="etienne.lyard@unige.ch",
+    data_files=[(
+        'lib/python3.5/site-packages', [
+            'rawzfitsreader.cpython-35m-x86_64-linux-gnu.so',
+            'libZFitsIO.so',
+            'libACTLCore.so',
+            'libprotobuf.so.9',
+            'libzmq.so.3',
+        ])
+    ],
+    install_requires=['numpy', 'protobuf'],
+    py_modules=['CoreMessages_pb2', 'L0_pb2', 'protozfitsreader'],
+)

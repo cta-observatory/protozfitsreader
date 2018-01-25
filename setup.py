68c58ebab5a7a57340efc210a8526c1f1e9948d4
Dominik Neise <neised@phys.ethz.ch>
2018-01-25 16:54:17 +0100

    bump minor version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='protozfitsreader',
     packages=['protozfitsreader'],
-    version='0.43.1',
+    version='0.43.2',
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",

eef2c5bd8d1750a0df4074a28308b8adae365243
Dominik Neise <neised@phys.ethz.ch>
2018-02-08 17:06:49 +0100

    switch linux back on and bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 setup(
     name='protozfitsreader',
     packages=['protozfitsreader'],
-    version='0.43.1',
+    version='0.44.0',
     description='Basic python bindings for protobuf zfits reader',
     author="Etienne Lyard et al.",
     author_email="etienne.lyard@unige.ch",
     package_data={
-        'protozfitsreader': ['*.so*', '*.dylib'],
+        'protozfitsreader': [
+            '*.so*',
+            '*.dylib'
+        ],
         '': ['tests/resources/*'],
     },
     install_requires=['numpy', 'protobuf'],

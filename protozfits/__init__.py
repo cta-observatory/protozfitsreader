2cdfec3df4c184e0e489dc176f34f241947ef19c
Dominik Neise <neised@phys.ethz.ch>
2018-03-03 18:12:15 +0100

    add some stuff to global namespace

--- protozfits/__init__.py
+++ protozfits/__init__.py
@@ -1,6 +1,8 @@
 from pkg_resources import resource_string
 from .digicam import File as DigicamFile
 from .simple import File as SimpleFile
+from .any_array_to_numpy import any_array_to_numpy
+from .simple import make_namedtuple
 
 __version__ = resource_string('protozfits', 'VERSION').decode().strip()
 
@@ -8,4 +10,6 @@ __version__ = resource_string('protozfits', 'VERSION').decode().strip()
 __all__ = [
     'DigicamFile',
     'SimpleFile',
+    'make_namedtuple',
+    'any_array_to_numpy',
 ]

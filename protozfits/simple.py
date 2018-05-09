9c66ce108edfdb2939f1291258308d40b379f4b6
Dominik Neise <neised@phys.ethz.ch>
2018-05-09 18:31:47 +0200

    Pushed rawzfitsreader(v 1.0) binaries for Python 3.6 on Linux

--- protozfits/simple.py
+++ protozfits/simple.py
@@ -10,6 +10,16 @@ from . import R1_pb2
 from .CoreMessages_pb2 import AnyArray
 from .any_array_to_numpy import any_array_to_numpy
 
+pb2_modules = {
+    'R1': R1_pb2,
+    'L0': L0_pb2,
+}
+
+
+def get_class_from_PBFHEAD(pbfhead):
+    module_name, class_name = pbfhead.split('.')
+    return getattr(pb2_modules[module_name], class_name)
+
 
 class File:
     def __init__(self, path, pure_protobuf=False):
@@ -32,7 +42,6 @@ BinTableDescription = namedtuple(
         'extname',
         'pbfhead',
         'znaxis2',
-        'pb_class_name',
         'header',
     ]
 )
@@ -47,7 +56,6 @@ def detect_bintables(path):
             extname=hdu.header['EXTNAME'],
             pbfhead=hdu.header['PBFHEAD'],
             znaxis2=hdu.header['ZNAXIS2'],
-            pb_class_name=hdu.header['PBFHEAD'].split('.')[-1],
             header=hdu.header
         )
         for hdu_id, hdu in enumerate(fitsfile)
@@ -74,7 +82,7 @@ class Table:
         desc: BinTableDescription
         '''
         self.__desc = desc
-        self.__pbuf_class = getattr(L0_pb2, desc.pb_class_name)
+        self.__pbuf_class = get_class_from_PBFHEAD(desc.pbfhead)
         self.header = self.__desc.header
         self.pure_protobuf = pure_protobuf
 
@@ -100,7 +108,7 @@ class Table:
             raise StopIteration
 
     def __repr__(self):
-        return '{cn}({d.znaxis2}x{d.pb_class_name})'.format(
+        return '{cn}({d.znaxis2}x{d.pbfhead})'.format(
             cn=self.__class__.__name__,
             d=self.__desc
         )
@@ -124,12 +132,13 @@ def message_getitem(msg, name):
         value = make_namedtuple(value)
     return value
 
+messages = set()
+for module in pb2_modules.values():
+    for name in dir(module):
+        thing = getattr(module, name)
+        if isinstance(thing, GeneratedProtocolMessageType):
+            messages.add(thing)
 
-messages = set([
-    getattr(L0_pb2, name)
-    for name in dir(L0_pb2)
-    if isinstance(getattr(L0_pb2, name), GeneratedProtocolMessageType)
-])
 
 def namedtuple_repr2(self):
     '''a nicer repr for big namedtuples containing big numpy arrays'''

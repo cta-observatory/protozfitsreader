eeb5c31f2d0ff7397275a7f5ad6a882570ac9221
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 09:37:35 +0200

    just import digicam ... not doing anything with it

--- protozfits/__init__.py
+++ protozfits/__init__.py
@@ -1,4 +1,5 @@
 from pkg_resources import resource_string
+import digicam
 from .simple import File as SimpleFile
 from .any_array_to_numpy import any_array_to_numpy
 from .simple import make_namedtuple

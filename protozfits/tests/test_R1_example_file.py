8ef2d78c2cac7babdc6574ff3c54fa04818ba23d
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 15:56:25 +0200

    allow the name to be DataModel as well (#43)
    * allow the name to be DataModel as well
    
    * add a test, to see if we can read all the files we have with File
    
    * print a bit less, but show me that you are really running

--- protozfits/tests/test_R1_example_file.py
+++ protozfits/tests/test_R1_example_file.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pkg_resources
 import os
+from glob import glob
 
 from protozfits import File
 
@@ -27,3 +28,23 @@ def test_can_iterate_over_events_and_run_header():
             assert event.pixel_status.shape == (14,)
             assert event.lstcam.first_capacitor_id.shape == (16,)
             assert event.lstcam.counters.shape == (44,)
+
+
+glob_path = pkg_resources.resource_filename(
+    'protozfits',
+    os.path.join(
+        'tests',
+        'resources',
+        '*.fits.fz'
+    )
+)
+
+all_test_resources = sorted(glob(glob_path))
+
+
+def test_can_open_and_get_an_event_from_all_test_resources():
+    print()
+    for path in all_test_resources:
+        with File(path) as f:
+            event = next(f.Events)
+        print(path, len(str(event)))

8831475328aff83a9506cf46c485e9b632ef6ec3
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 09:51:59 +0200

    I try to import, what digicam.py imported

--- protozfits/__init__.py
+++ protozfits/__init__.py
@@ -1,5 +1,18 @@
 from pkg_resources import resource_string
-from . import digicam as do_not_use_me
+
+# imports formerly done in digicam.py
+from os.path import isfile
+import warnings
+import numpy as np
+from astropy.utils.decorators import lazyproperty
+
+from . import rawzfitsreader
+from google.protobuf.pyext.cpp_message import GeneratedProtocolMessageType
+from .patch_ids import PATCH_ID_INPUT_SORT_IDS, PATCH_ID_OUTPUT_SORT_IDS
+from .any_array_to_numpy import any_array_to_numpy
+from .L0_pb2 import CameraEvent
+# end of imports from digicam.py
+
 from .simple import File as SimpleFile
 from .any_array_to_numpy import any_array_to_numpy
 from .simple import make_namedtuple
